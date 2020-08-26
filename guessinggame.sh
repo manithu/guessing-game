30badff9d1413de81c268b0b88c0c829624e2d0c
Manithu <manithu@gmail.com>
2020-08-26 17:39:25 +0200

    formatted text

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,29 +1,26 @@
 #!/usr/bin/env bash
 #File: guessinggame.sh
-
 function nfiles {
 find . -type f -depth 1 | wc -l
 #only file no directory depth 1 to only count current directory
 }
-
 cnt=1
 true=$(nfiles)
 
 while [[ $cnt -gt 0 ]]
 do
- echo "Guess, how many files are in the current directory?"
+ echo "How many files are in the current directory?"
  read guess
 
-  if [[ $guess -eq $true ]]
+ if [[ $guess -eq $true ]]
 	then 
-	 echo "Correct!!The force is strong in you!"
-	 let cnt=0
+	echo "Correct!!The force is strong with you!"
+	let cnt=0
   elif [[ $guess -gt $true ]]
 	then 
 	echo "Your number is too high."
   elif [[ $guess -lt $true ]]
 	then 
 	echo "Your number is too low."
- 
-  fi
+ fi
 done
